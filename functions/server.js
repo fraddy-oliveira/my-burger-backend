4a3db74f1ce29533ad17a8a95ed9c56e74cf4db1
Fraddy Oliveira <fraddy.dev@gmail.com>
2024-06-17 11:52:46 +0530

    fix: netlify deployment

--- functions/server.js
+++ functions/server.js
@@ -1,19 +1,22 @@
 var createError = require("http-errors");
 var express = require("express");
 var logger = require("morgan");
+var cors = require("cors");
 var serverless = require("serverless-http");
 
-var indexRouter = require("./routes/index");
-var orderRouter = require("./routes/order");
+var indexRouter = require("../routes/index");
+var orderRouter = require("../routes/order");
 
 var app = express();
+
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", indexRouter);
-app.use("/order", orderRouter);
+app.use("/.netlify/functions/server", indexRouter);
+app.use("/.netlify/functions/server/order", orderRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
